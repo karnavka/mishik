b1ad102403d8c4a651c97bc6287e765835dabcf2
Vira Vznk <veravznk@gmail.com>
2026-05-27 18:34:04 +0300

    maven

--- backend/src/main/java/com/mishik/backend/BackendApplication.java
+++ backend/src/main/java/com/mishik/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.misik.backend;
+package com.mishik.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/mishik/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/mishik/backend/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.misik.backend;
+package com.mishik.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
