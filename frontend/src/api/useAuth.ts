245ec291d610d169d294cb5459662da246636960
Vira Vznk <veravznk@gmail.com>
2026-05-30 13:07:35 +0300

    Login front 1

---
+++ frontend/src/api/useAuth.ts
@@ -0,0 +1,15 @@
+import { getRole, isLoggedIn } from '../utils/auth';
+
+export const useAuth = () => {
+  const loggedIn = isLoggedIn();
+  const role = getRole();
+
+  return {
+    loggedIn,
+    role,
+    isAdmin: role === 'ADMIN',
+    isModerator: role === 'MODERATOR' || role === 'ADMIN',
+    isUser: role === 'USER',
+    isShelter: role === 'SHELTER',
+  };
+};
