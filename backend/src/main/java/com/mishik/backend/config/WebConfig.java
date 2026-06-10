48b32ce91a64330f1288f635be349e4eebe1a1a2
aniakasol <annakrasn2212@gmail.com>
2026-06-11 02:20:53 +0300

    animal detailed info page + added pictures

---
+++ backend/src/main/java/com/mishik/backend/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.mishik.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/images/**")
+//                .addResourceLocations("file:uploads/images/");
+//    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String uploadPath = System.getProperty("user.dir") + "/uploads/images/";
+        System.out.println(">>> Serving images from: " + uploadPath);
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:" + uploadPath);
+    }
+}
