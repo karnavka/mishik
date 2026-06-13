2722798ae2080687f512cb825ed811c89baf1435
aniakasol <annakrasn2212@gmail.com>
2026-06-13 18:29:48 +0300

    css LETSGOOO (i died)

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,18 @@
+
+
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className= 'footer-row'>
+                <img src="/src/images/dognobgcolor.png" className="footer-img" alt="Mishik"/>
+                <img src="/src/images/logo.png" className="header-logo" alt="Mishik"/>
+                <img src="/src/images/catnobgcolor.png" className="footer-img" alt="Mishik"/>
+            </div>
+            <div className="footer-last">
+                <span></span>
+                <span>       шукай пухнатих друзів разом з нами</span>
+                <span>© 2026</span>
+            </div>
+        </footer>
+    );
+}
